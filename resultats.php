0c839fd91441e2c7649804a105a32e5b0871fea8
Emilie <emilie.chnvt@gmail.com>
2025-01-11 16:54:03 +0100

    bravo + dexconnexion displayed

--- resultats.php
+++ resultats.php
@@ -1,3 +1,16 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Succes</title>
+</head>
+<body>
 
-echo "<h1>Bravo</h1>";
+<div class="container d-flex flex-column align-items-center justify-content-center">
+    <h1>Bravo</h1>
+    <a href="index.php">Déconnexion</a>
+
+</div>
+</body>
+</html>
