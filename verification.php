4727d0f0644aba482824db02273472d001611d2f
Emilie <emilie.chnvt@gmail.com>
2025-01-11 16:35:54 +0100

    link index.php added

--- verification.php
+++ verification.php
@@ -1,1 +1,22 @@
 <?php
+
+$users = [
+    "luc"=> "motDePasseDeLuc4",
+    "michel"=> "pasteque",
+    "eglantine"=> "choucroute",
+    "patricia"=> "surf"
+];
+
+$name = null;
+$pswd = null;
+
+if(!empty($_POST["name"])){
+    $name = $_POST["name"];
+}
+
+if(!empty($_POST["pswd"])){
+    $pswd = $_POST["pswd"];
+}
+?>
+<a href="index.php">ddeconnexion</a>
+
